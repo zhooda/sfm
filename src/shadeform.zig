5f7ff83051886f4ce4fb1270eb2bfb37f48bdf4a
zhooda <zee@sparselabs.org>
2024-08-22 13:15:54 -0600

    feat(lib): add /info and /delete for instances

--- src/shadeform.zig
+++ src/shadeform.zig
@@ -41,46 +41,55 @@ pub const Instance = struct {
     cloud_assigned_id: []const u8,
     shade_cloud: bool,
     name: []const u8,
-    configuration: struct {
+    configuration: Configuration,
+    ip: ?[]const u8, // optional
+    ssh_user: []const u8,
+    ssh_port: ?u16, // extra, optional
+    status: []const u8,
+    status_details: []const u8, // extra
+    cost_estimate: []const u8,
+    hourly_price: ?u32,
+    launch_configuration: ?LaunchConfiguration = null,
+    created_at: []const u8,
+    deleted_at: ?[]const u8, // optional
+    details: ?struct{} = null,
+    ssh_key_id: []const u8, // extra
+
+    pub const Configuration = struct {
         memory_in_gb: u16,
         storage_in_gb: u32,
         vcpus: u16,
         num_gpus: u8,
         gpu_type: []const u8,
         interconnect: []const u8,
-        vram_per_gpu_in_gb: u16,
+        nvlink: bool, // extra
         os: []const u8,
-    },
-    ip: []const u8,
-    ssh_user: []const u8,
-    status: []const u8,
-    cost_estimate: []const u8,
-    hourly_price: u32,
-    launch_configuration: struct {
+        vram_per_gpu_in_gb: u16,
+    };
+
+    pub const LaunchConfiguration = struct {
         type: []const u8,
-        docker_configuration: struct {
+        docker_configuration: ?struct {
             image: []const u8,
-            args: []const u8,
-            shared_memory_in_gb: u16,
-            envs: []struct {
+            args: ?[]const u8,
+            shared_memory_in_gb: ?u16,
+            envs: ?[]struct {
                 name: []const u8,
                 value: []const u8,
             },
-            post_mappings: []struct {
+            post_mappings: ?[]struct {
                 host_port: u16,
                 container_port: u16,
             },
-            volume_mounts: []struct {
+            volume_mounts: ?[]struct {
                 host_path: []const u8,
                 container_path: []const u8,
             },
         },
-        script_configuration: struct {
+        script_configuration: ?struct {
             base64_script: []const u8,
         },
-    },
-    created_at: []const u8,
-    deleted_at: []const u8,
+    };
 };
 
 pub const CreateInstanceReq = struct {
@@ -130,20 +139,40 @@ pub const Client = struct {
     _bbuf: []u8 = undefined,
     _req_opts: std.http.Client.RequestOptions = undefined,
 
-    pub fn makeRequest(self: *Self, endpoint: []const u8) !std.http.Client.Request {
+    pub fn makeRequest(
+        self: *Self,
+        method: std.http.Method,
+        endpoint: []const u8,
+        options: struct { body: ?[]const u8 = null },
+    ) !std.http.Client.Request {
         self.uri.path = std.Uri.Component{ .raw = endpoint };
         self._req_opts = std.http.Client.RequestOptions{
             .server_header_buffer = &self._hbuf,
             .extra_headers = self.headers,
         };
+
         var request = try self.client.open(
-            std.http.Method.GET,
+            method,
             self.uri,
             self._req_opts,
         );
         errdefer request.deinit();
 
+        const body = options.body orelse &[_]u8{};
+
+        // set content length
+        // TODO: RequestTransfer.none may be suitable for empty POSTs
+        if (method == .POST)
+            request.transfer_encoding = .{ .content_length = body.len };
+
         try request.send();
+
+        // write body if required
+        switch (method) {
+            .POST => if (options.body) |value| try request.writeAll(value),
+            else => {},
+        }
+
         try request.finish();
         try request.wait();
 
@@ -173,7 +202,7 @@ pub const Client = struct {
     }
 
     pub fn getInstanceTypes(self: *Self) ![]InstanceType {
-        var request = try self.makeRequest("/v1/instances/types");
+        var request = try self.makeRequest(.GET, "/v1/instances/types", .{});
 
         try std.testing.expectEqual(request.response.status, .ok);
 
@@ -194,7 +223,7 @@ pub const Client = struct {
     }
 
     pub fn getInstances(self: *Self) ![]Instance {
-        var request = try self.makeRequest("/v1/instances");
+        var request = try self.makeRequest(.GET, "/v1/instances", .{});
 
         try std.testing.expectEqual(request.response.status, .ok);
 
@@ -211,5 +240,50 @@ pub const Client = struct {
         );
 
         return parsed.instances;
+    }
+
+    pub fn deleteInstance(self: *Self, instance_id: []const u8) !void {
+        var path: [64]u8 = .{0} ** 64;
+        _ = try std.fmt.bufPrintZ(
+            @as([]u8, &path),
+            "/v1/instances/{s}/delete",
+            .{instance_id},
+        );
+        const request = try self.makeRequest(
+            .POST,
+            std.mem.sliceTo(&path, 0),
+            .{},
+        );
+
+        try std.testing.expectEqual(.ok, request.response.status);
+    }
+
+    pub fn getInstance(self: *Self, instance_id: []const u8) !Instance {
+        var path: [64]u8 = .{0} ** 64;
+        _ = try std.fmt.bufPrintZ(
+            @as([]u8, &path),
+            "/v1/instances/{s}/info",
+            .{instance_id},
+        );
+        var request = try self.makeRequest(
+            .GET,
+            std.mem.sliceTo(&path, 0),
+            .{},
+        );
+
+        try std.testing.expectEqual(request.response.status, .ok);
+
+        var rdr = request.reader();
+        self._bbuf = try rdr.readAllAlloc(self.allocator, body_max_size);
+        errdefer self.allocator.free(self._bbuf);
+
+        const parsed = try std.json.parseFromSliceLeaky(
+            Instance,
+            self.allocator,
+            self._bbuf,
+            .{},
+        );
+
+        return parsed;
     }
 };
